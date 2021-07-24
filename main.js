2738e0ef158c87c122d115f6fff80ee6185396c4
chrisdaid <c.chrisdb@gmail.com>
2021-07-24 02:21:13 -0400

    fixed grid layout with pictures and changed color scheme + added footer

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const topBtn = document.getElementById('top-btn');
 const topBtnBg = document.getElementById('top-btn-bg');
 
 
-topBtn.addEventListener("mouseover", () => topBtnBg.style.backgroundColor = "rgba(50, 41, 47, 0.75)")
+topBtn.addEventListener("mouseover", () => topBtnBg.style.backgroundColor = "rgba(50, 41, 47, 0.75)");
 
-topBtn.addEventListener("mouseout", () => topBtnBg.style.backgroundColor = "")
+topBtn.addEventListener("mouseout", () => topBtnBg.style.backgroundColor = "");
